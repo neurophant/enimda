47017ece73c19a22e99ba96117809d3955c04557
Anton Smolin <smolin.anton@gmail.com>
2016-06-19 21:53:07 +0300

    package stuff

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='enimda',
+    version='1.0.0b1',
+    description='ENtropy-based IMage border Detection Algorithm',
+    long_description=long_description,
+    url='https://github.com/embali/enimda/',
+    author='Anton Smolin',
+    author_email='smolin.anton@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='image border detection',
+    py_modules=['enimda'],
+    install_requires=['numpy>=1.11.0', 'Pillow>=3.2.0'],
+)
